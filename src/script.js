c9df8c54b20b0926de5b41e03a8fa4b57d395ef2
r1nion <r1nion353@gmail.com>
2025-01-04 01:55:22 +0900

    samazama

--- src/script.js
+++ src/script.js
@@ -28,7 +28,7 @@ form.addEventListener("submit", (event) => {
 
 hintButton.addEventListener("click", () => {
   if (hint.textContent === "") {
-    hint.textContent = "ヒント:\n黒=0, 白=1で隠れてます。\nどうしてもわからなかったらLINEしてね☆\n\nASCII：7bitずつで一文字を表現する文字コード\nシーザー暗号：一定でずらして暗号化し、暗号文を同じだけ逆にずらして復号する暗号方法。";
+    hint.textContent = "ヒント:\n黒=0, 白=1で隠れてます。\nピタゴラス数3:4:5を使うのでは､､､？！\n\nどうしてもわからなかったらLINEしてね☆\n\nASCII：7bitずつで一文字を表現する文字コード\nシーザー暗号：一定でずらして暗号化し、暗号文を同じだけ逆にずらして復号する暗号方法。";
     hintButton.textContent = "ヒントを非表示";
   } else {
     hint.textContent = "";
